ffaecf5eb843cedd029ca375a1a95b0ec0731982
PracaOl <100939454+PracaOl@users.noreply.github.com>
2022-10-14 15:14:01 +0200

    Update Form1.cs

--- WinFormsTaskOP4Hp/Form1.cs
+++ WinFormsTaskOP4Hp/Form1.cs
@@ -7,6 +7,9 @@ namespace WinFormsTaskOP4Hp
         string AdresIP;
         string[] IPNumbers;
         int[] IPNumbersInt;
+        string AdresIPClass;
+        int AdresIPClassInt;
+        int abc = 0;
         public Form1()
         {
             InitializeComponent();
@@ -15,19 +18,50 @@ namespace WinFormsTaskOP4Hp
         private void button1_Click(object sender, EventArgs e)
         {
             AdresIP = textBox1.Text;
-            IPNumbers = AdresIP.Split('.');
+            if (AdresIP.Contains("."))
+            {
+                AdresIPClass = AdresIP.Substring(0, AdresIP.IndexOf("."));
+            }
+            AdresIPClassInt = Int32.Parse(AdresIPClass);
+            IPNumbers = AdresIP.Split(".");
             IPNumbersInt = Array.ConvertAll(IPNumbers, int.Parse);
             for (int i = 0; i < IPNumbersInt.Length; i++)
             {
                 if (IPNumbersInt[i] > 0 && 256 > IPNumbersInt[i])
                 {
-                    label3.Text = "Czy Poprawny Adres IP: " + "TAK" + IPNumbersInt;
+                    abc++;
                 }
-                else
+                if (abc == 4)
                 {
-                    label3.Text = "Czy Poprawny Adres IP: " + "NIE" + IPNumbersInt;
+                    label3.Text = "Czy Poprawny Adres IP: " + "TAK" + AdresIPClass + abc;
+                }
+                if (abc != 4)
+                {
+                    label3.Text = "Czy Poprawny Adres IP: " + "NIE" + AdresIPClass + abc;
                 }
             }
+            abc = 0;
+            if (AdresIPClassInt >= 1 && 127 >= AdresIPClassInt)
+            {
+                label4.Text = "Klasa Sieci: A";
+            }
+            if (AdresIPClassInt >= 128 && 191 >= AdresIPClassInt)
+            {
+                label4.Text = "Klasa Sieci: B";
+            }
+            if (AdresIPClassInt >= 192 && 223 >= AdresIPClassInt)
+            {
+                label4.Text = "Klasa Sieci: C";
+            }
+            if (AdresIPClassInt >= 224 && 239 >= AdresIPClassInt)
+            {
+                label4.Text = "Klasa Sieci: D";
+            }
+            if (AdresIPClassInt >= 240 && 255 >= AdresIPClassInt)
+            {
+                label4.Text = "Klasa Sieci: E";
+            }
+            // Dodaj Label podnazwa "label4". Add Label under a name "label4". Windows Forms!!
         }
     }
 }
